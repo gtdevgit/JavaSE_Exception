e4985d31f0120cd01e6bd974695651ecabbc0395
Guillaume Toussaint <gtdev.git@gmail.com>
2022-02-08 11:46:33 +0100

    Exo 2.3 et 2.4

--- src/com/training/exception/Main.java
+++ src/com/training/exception/Main.java
@@ -5,17 +5,25 @@ import java.util.InputMismatchException;
 public class Main {
 
     public static void main(String[] args)   {
-	// write your code here
-        try{
-            int age = getAgeCap();
-            System.out.println("Age = " + age);
-        }
-        catch(AgeCapException e) {
-            System.err.println(e.getMessage());
-        }
+        boolean ageincoreecte = true;
+        int counterror = 0;
+
+        do{
+            try{
+                int age = getAgeCap();
+                ageincoreecte = false;
+                System.out.println("Age = " + age);
+            }
+            catch(Exception e) {
+                System.err.println(e.getMessage());
+                counterror++;
+                System.err.println("Nb essais en erreur = " + counterror);
+            }
+        } while (ageincoreecte);
+
     }
 
-    private static int getAgeCap() throws AgeCapException {
+    private static int scanAgeCap() throws AgeCapException {
         String value = null;
         try {
             value = ScannerUtils.getString("Quel est l'age du capitaine ?");
@@ -26,6 +34,15 @@ public class Main {
             return age;
         } catch (NumberFormatException nfe) {
             throw new AgeCapException(value);
+        }
+    }
+
+    private static int getAgeCap() throws Exception {
+        try {
+            return scanAgeCap();
+        }
+        catch (AgeCapException e) {
+            throw new Exception(e.getMessage());
         }
     }
 }
